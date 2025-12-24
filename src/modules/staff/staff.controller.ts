fcccc7e89c3957c91a6dfdce65cefa9be13905a4
RajeevSingh62 <Rajeevsingh629944@gmail.com>
2025-12-24 23:43:06 +0530

    staff controller crud

--- src/modules/staff/staff.controller.ts
+++ src/modules/staff/staff.controller.ts
@@ -8,7 +8,7 @@ export const  createStaff = async (req: Request, res: Response) => {
     } catch (err: any) {
       return res.status(500).json({ success: false, message: err.message });
     }
-  }
+  };
 
   export const getAllStaff=async (req: Request, res: Response) => {
     try {
@@ -20,34 +20,34 @@ export const  createStaff = async (req: Request, res: Response) => {
     }
   }
 
-//   getStaff: async (req: Request, res: Response) => {
-//     try {
-//       const staff = await Staff.findById(req.params.id);
-//       return res.status(200).json({ success: true, staff });
-//     } catch (err: any) {
-//       return res.status(404).json({ success: false, message: "Not found" });
-//     }
-//   },
+  export const getStaff= async (req: Request, res: Response) => {
+    try {
+      const staff = await Staff.findById(req.params.id);
+      return res.status(200).json({ success: true, staff });
+    } catch (err: any) {
+      return res.status(404).json({ success: false, message: "Not found" });
+    }
+  }
 
-//   updateStaff: async (req: Request, res: Response) => {
-//     try {
-//       const staff = await Staff.findByIdAndUpdate(req.params.id, req.body, {
-//         new: true,
-//       });
-//       return res.status(200).json({ success: true, staff });
-//     } catch (err: any) {
-//       return res.status(500).json({ success: false, message: err.message });
-//     }
-//   },
+  export const updateStaff=async (req: Request, res: Response) => {
+    try {
+      const staff = await Staff.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+      });
+      return res.status(200).json({ success: true, staff });
+    } catch (err: any) {
+      return res.status(500).json({ success: false, message: err.message });
+    }
+  }
 
-//   deleteStaff: async (req: Request, res: Response) => {
-//     try {
-//       await Staff.findByIdAndDelete(req.params.id);
-//       return res
-//         .status(200)
-//         .json({ success: true, message: "Staff deleted" });
-//     } catch (err: any) {
-//       return res.status(500).json({ success: false, message: err.message });
-//     }
-//   },
-// };
+  export const deleteStaff=async (req: Request, res: Response) => {
+    try {
+      await Staff.findByIdAndDelete(req.params.id);
+      return res
+        .status(200)
+        .json({ success: true, message: "Staff deleted" });
+    } catch (err: any) {
+      return res.status(500).json({ success: false, message: err.message });
+    }
+  }
+
--- src/modules/staff/staff.routes.ts
+++ src/modules/staff/staff.routes.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import {createStaff, getAllStaff} from "./staff.controller";
+import {createStaff, deleteStaff, getAllStaff, getStaff, updateStaff} from "./staff.controller";
 
 const router = Router();
 
 router.post("/",createStaff);
 router.get("/", getAllStaff);
-// router.get("/:id", staffController.getStaff);
-// router.put("/:id", staffController.updateStaff);
-// router.delete("/:id", staffController.deleteStaff);
+router.get("/:id",getStaff);
+router.put("/:id", updateStaff);
+router.delete("/:id", deleteStaff);
 
 export default router;
