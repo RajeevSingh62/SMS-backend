df5c4615cc5379d2c38780f18a76bd9140a02ffa
Rajeev Singh <105496348+RajeevSingh62@users.noreply.github.com>
2025-12-30 15:16:33 +0530

    Update index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,7 +6,8 @@ import staffRoutes from "../modules/staff/staff.routes";
 import studentDocRoutes from "../modules/studentDoc/studemtdoc.route";
 import parentRoutes from "../modules/parent/parent.route";
 import attendanceRoutes from "../modules/attendance/attendance.route";
-import sectionRoutes from "../modules/section/section.routes"
+import sectionRoutes from "../modules/section/section.routes";
+import classRoutes from "../modules/classes/classes.routes"
 const router = Router();
 
 router.use("/auth", authRoutes);
@@ -19,6 +20,7 @@ router.use("/parent",parentRoutes);
 router.use("/attendance",attendanceRoutes);
 router.use("/attendance",attendanceRoutes);
 router.use("/section",sectionRoutes);
+router.use("/class",classRoutes);
 
 export default router;
  
